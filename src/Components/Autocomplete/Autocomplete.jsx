a486ee965b8e87c4c007dc25780837f6fc9d02cc
evgenii-shvetsov <sea.dev91@gmail.com>
2023-04-23 19:14:27 -0700

    add multiple words handling functionality

--- src/Components/Autocomplete/Autocomplete.jsx
+++ src/Components/Autocomplete/Autocomplete.jsx
@@ -1,58 +1,95 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import PropTypes from "prop-types";
 import Trie from "../Trie/Trie";
 import "./Autocomplete.css";
 
 const Autocomplete = ({ trie }) => {
+  const [selectedWords, setSelectedWords] = useState([]);
   const [inputValue, setInputValue] = useState("");
   const [suggestions, setSuggestions] = useState([]);
   const [activeSuggestionIndex, setActiveSuggestionIndex] = useState(-1);
-  const isSelected = inputValue !== "" && !suggestions.length;
 
   const handleInputChange = (e) => {
     const value = e.target.value;
     setInputValue(value);
     setActiveSuggestionIndex(-1);
-    if(value.length > 0){
-        setSuggestions(trie.autoComplete(value));
+    if (value.length > 0) {
+      setSuggestions(trie.autoComplete(value));
     } else {
-        setSuggestions([]);
+      setSuggestions([]);
     }
   };
 
   const handleSuggestionsClick = (suggestion) => {
-    setInputValue(suggestion);
+    setSelectedWords([...selectedWords, suggestion]);
+    setInputValue("");
     setSuggestions([]);
+    setActiveSuggestionIndex(-1);
   };
 
-  const handleKeyDown = (e) => {
-    if (e.key === "ArrowDown") {
+
+const handleKeyDown = (e) => {
+  if (e.key === "ArrowDown") {
+    setActiveSuggestionIndex((prevIndex) =>
+      prevIndex < suggestions.length - 1 ? prevIndex + 1 : prevIndex
+    );
+  } else if (e.key === "ArrowUp") {
+    setActiveSuggestionIndex((prevIndex) =>
+      prevIndex > 0 ? prevIndex - 1 : prevIndex
+    );
+  } else if (e.key === "Enter") {
+    if (activeSuggestionIndex > -1) {
+      handleSuggestionsClick(suggestions[activeSuggestionIndex]);
       e.preventDefault();
-      setActiveSuggestionIndex((prevIndex) =>
-        Math.min(prevIndex + 1, suggestions.length - 1)
-      );
-    } else if (e.key === "ArrowUp") {
-      e.preventDefault();
-      setActiveSuggestionIndex((prevIndex) => Math.max(prevIndex - 1, -1));
-    } else if (e.key === "Enter" && activeSuggestionIndex !== -1) {
-      setInputValue(suggestions[activeSuggestionIndex]);
-      setSuggestions([]);
-      setActiveSuggestionIndex(-1);
     }
-  };
+  }
+};
 
-  
+  const isSelected = inputValue !== "" && !suggestions.length;
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    if (isSelected) {
+      inputRef.current.focus();
+    }
+  }, [isSelected]);
+
+
+const handleDeleteClick = (index) => {
+  setSelectedWords([
+    ...selectedWords.slice(0, index),
+    ...selectedWords.slice(index + 1),
+  ]);
+  inputRef.current.focus();
+};
+
   return (
     <div className="autocomplete-container">
-      <input
-        // className="autocomplete-input"
-        className={`autocomplete-input ${isSelected ? "selected" : ""}`}
-        type="text"
-        value={inputValue}
-        onChange={handleInputChange}
-        onKeyDown={handleKeyDown}
-        placeholder="Start typing..."
-      />
+      <div className={`input-wrapper ${isSelected ? "selected" : ""}`}>
+        {selectedWords.map((selectedWord, index) => (
+          <div key={index} className="selected-word-box">
+            <span>{selectedWord}</span>
+            <button
+              className="delete-button"
+              onClick={() => handleDeleteClick(index)}
+            >
+              x
+            </button>
+          </div>
+        ))}
+        {selectedWords.length < 3 && (
+          <input
+            type="text"
+            value={inputValue}
+            onChange={handleInputChange}
+            onKeyDown={handleKeyDown}
+            placeholder="Start typing..."
+            ref={inputRef}
+            className="autocomplete-input"
+          />
+        )}
+      </div>
+
       <ul className="suggestions-dropdown">
         {suggestions.map((suggestion, index) => (
           <li
@@ -72,6 +109,5 @@ const Autocomplete = ({ trie }) => {
 Autocomplete.propTypes = {
   trie: PropTypes.instanceOf(Trie).isRequired,
 };
-
 
 export default Autocomplete;
