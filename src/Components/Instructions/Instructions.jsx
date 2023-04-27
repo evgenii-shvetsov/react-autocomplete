902f6ba3bf3dba9fd6e0f7a2f83d7c3fecba1c90
evgenii-shvetsov <sea.dev91@gmail.com>
2023-04-27 10:42:13 -0700

    modified instructions module

--- src/Components/Instructions/Instructions.jsx
+++ src/Components/Instructions/Instructions.jsx
@@ -21,45 +21,47 @@ const Instructions = () => {
           <div className="overlay" onClick={handleClickOutside}></div>
           <div className="info-block">
             <p>
-              📚 Trie (also known as a prefix tree, the correct pronunciation is
+              📚 <strong>Trie</strong>  (also known as a prefix tree, the correct pronunciation is
               “try”) is a tree-like data structure that stores a dynamic set of
               strings. It's particularly useful for applications involving
               searching, sorting, and storing strings with shared prefixes, such
               as dictionaries and autocomplete functionalities.
             </p>
             <p>
-              In Demo purposes, I used 78 words, three words per each letter in
+              For Demo purposes, I used 78 words, three words per each letter in
               the English Alphabet
             </p>
-            <p>
-              This is the list of words in the Trie: <br />
-              a: ["algorithm", "application", "artificial"], <br />
-              b: ["backup", "binary", "browser"], <br />
-              c: ["compiler", "code", "cybersecurity"], <br />
-              d: ["data", "debugging", "desktop"], <br />
-              e: ["encryption", "ethernet", "email"], <br />
-              f: ["firewall", "firmware", "file"], <br />
-              g: ["gui", "gaming", "gigabyte"], <br />
-              h: ["hacker", "harddrive", "html"], <br />
-              i: ["input", "interface", "internet"], <br />
-              j: ["java", "javascript", "jpeg"], <br />
-              k: ["kernel", "keyboard", "kilobyte"], <br />
-              l: ["laptop", "linux", "logic"], <br />
-              m: ["monitor", "motherboard", "mouse"], <br />
-              n: ["network", "node", "notebook"], <br />
-              o: ["object", "optical", "output"], <br />
-              p: ["password", "processor", "protocol"], <br />
-              q: ["query", "quicktime", "quantum"], <br />
-              r: ["ram", "router", "robotics"], <br />
-              s: ["software", "server", "storage"], <br />
-              t: ["trojan", "terminal", "tablet"], <br />
-              u: ["uniform", "universals", "unix"], <br />
-              v: ["virus", "virtualization", "video"], <br />
-              w: ["website", "windows", "wifi"], <br />
-              x: ["xbox", "xml", "xerox"], <br />
-              y: ["yaml", "yottabyte","youtube"], <br />
-              z: ["zip", "zettabyte", "zoom"]
-            </p>
+            <div className="trie-words-block">
+              <strong> This are the words in the Trie:</strong> <br />
+              <p id="words-object">
+                a: ["algorithm", "application", "artificial"] <br />
+                b: ["backup", "binary", "browser"] <br />
+                c: ["compiler", "code", "cybersecurity"] <br />
+                d: ["data", "debugging", "desktop"] <br />
+                e: ["encryption", "ethernet", "email"] <br />
+                f: ["firewall", "firmware", "file"] <br />
+                g: ["gui", "gaming", "gigabyte"] <br />
+                h: ["hacker", "harddrive", "html"] <br />
+                i: ["input", "interface", "internet"] <br />
+                j: ["java", "javascript", "jpeg"] <br />
+                k: ["kernel", "keyboard", "kilobyte"] <br />
+                l: ["laptop", "linux", "logic"] <br />
+                m: ["monitor", "motherboard", "mouse"] <br />
+                n: ["network", "node", "notebook"] <br />
+                o: ["object", "optical", "output"] <br />
+                p: ["password", "processor", "protocol"] <br />
+                q: ["query", "quicktime", "quantum"] <br />
+                r: ["ram", "router", "robotics"] <br />
+                s: ["software", "server", "storage"] <br />
+                t: ["trojan", "terminal", "tablet"] <br />
+                u: ["uniform", "universals", "unix"] <br />
+                v: ["virus", "virtualization", "video"] <br />
+                w: ["website", "windows", "wifi"] <br />
+                x: ["xbox", "xml", "xerox"] <br />
+                y: ["yaml", "yottabyte","youtube"] <br />
+                z: ["zip", "zettabyte", "zoom"]
+              </p>
+            </div>
           </div>
         </>
       )}
